ab1a7c8f8e6e3d411bd9b38660e685ce02b450d7
Sam Merrell <merrell.sam@gmail.com>
2015-08-08 18:34:18 -0600

    Read lines of a file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate argparse;
 
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
 use argparse::{ArgumentParser, StoreTrue, Store};
 
 fn main() {
@@ -26,5 +29,14 @@ fn main() {
         println!("Searching file {} with pattern {}", filename, pattern);
     }
 
-    println!("{}", filename);
+    // try! isn't working here, not sure why
+    let file = match File::open(filename) {
+        Err(why) => panic!("couldn't read to string {}", &why),
+        Ok(file) => file,
+    };
+    let file = BufReader::new(file);
+
+    for line in file.lines() {
+        println!("{}", line.unwrap());
+    }
 }
