e7c494e6127c2fcd3a139169cbdb1ee3de5e8151
Sam Merrell <merrell.sam@gmail.com>
2015-08-09 15:45:21 -0600

    Move argument parsing into function
    Also use a struct instead of multiple variables, helps readability a bit
    I think.

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,63 @@ use std::fs::File;
 use argparse::{ArgumentParser, StoreTrue, Store};
 use regex::Regex;
 
-fn main() {
-    let mut verbose = false;
-    let mut filename = "".to_string();
-    let mut pattern = "".to_string();
-    let mut case_insensitive = false;
+struct Options {
+    verbose: bool,
+    case_insensitive: bool,
+    pattern: String,
+    filename: String
+}
+
+fn parse_arguments() -> Options {
+    let mut options = Options {
+        verbose: false,
+        case_insensitive: false,
+        pattern: "".to_string(),
+        filename: "".to_string(),
+    };
+
     {
         let mut ap = ArgumentParser::new();
-        ap.set_description("Greet somebody");
-        ap.refer(&mut verbose)
+        ap.set_description("Search through a file, like grep");
+        ap.refer(&mut options.verbose)
             .add_option(&["-v", "--verbose"], StoreTrue,
                     "verbose output");
-        ap.refer(&mut case_insensitive)
+        ap.refer(&mut options.case_insensitive)
             .add_option(&["-i", "--case-insensitive"], StoreTrue,
                     "case insensitive search");
-        ap.refer(&mut pattern)
+        ap.refer(&mut options.pattern)
             .add_argument("pattern", Store, "pattern")
             .required();
-        ap.refer(&mut filename)
+        ap.refer(&mut options.filename)
             .add_argument("file", Store, "file to search")
             .required();
 
         ap.parse_args_or_exit();
     }
 
-    if verbose {
-        println!("Searching file {} with pattern {}", &filename, &pattern);
+    options
+}
+
+fn main() {
+    let options = parse_arguments();
+    if options.verbose {
+        println!("Searching file {} with pattern {}", &options.filename, &options.pattern);
     }
 
     // try! isn't working here, not sure why
-    let file = match File::open(&filename) {
-        Err(why) => panic!("couldn't open {} {}", &filename, &why),
+    let file = match File::open(&options.filename) {
+        Err(why) => panic!("couldn't open {} {}", &options.filename, &why),
         Ok(file) => file,
     };
     let file = BufReader::new(file);
 
-    let insensive_pattern = if case_insensitive {
+    let insensive_pattern = if options.case_insensitive {
         "(?i)"
     } else {
         ""
     };
 
-    let full_pattern = insensive_pattern.to_string() + &pattern;
+    let full_pattern = insensive_pattern.to_string() + &options.pattern;
     let regex = Regex::new(&full_pattern).unwrap();
     for line in file.lines() {
         let l = line.unwrap().to_string();
